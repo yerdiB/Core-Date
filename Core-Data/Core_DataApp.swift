f3207fcf5dfefec6163cfe18a84512b4f9201e9a
yerdi <yerdi.isman@gmail.com>
2023-01-18 23:27:04 +0600

    first commit

---
+++ Core-Data/Core_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_DataApp.swift
+//  Core-Data
+//
+//  Created by Yerdaulet Ismanaliyev on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Core_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
